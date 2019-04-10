cbfa457c738c64857d8c156091dae05323fb0361
Gusbe <gusbertolo@gmail.com>
2019-04-10 23:13:03 +0200

    Game logic

--- src/memory.js
+++ src/memory.js
@@ -1,12 +1,42 @@
 var MemoryGame = function (cards) {
+  
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (cards) {
+
+  let shuffleCards = [];
+  while(cards.length > 0){
+    let randomIndex = Math.floor(Math.random() * cards.length);
+    let cardObject = {name: cards[randomIndex].name, img: cards[randomIndex].img}
+    shuffleCards.push(cardObject);
+    cards.splice(randomIndex, 1);
+  }
+  
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+
+  this.pairsClickedClicked++;
+  
+  if(firstCard.name === secondCard.name){
+    this.pairsGuessed++;
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 MemoryGame.prototype.isFinished = function () {
+
+  if(this.pairsGuessed === this.cards.length/2){
+    return true;
+  }
+  else{
+    return false;
+  }
 };
