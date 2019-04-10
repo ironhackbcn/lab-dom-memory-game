78b4bc13b3f25b3ed29ae8744d316d9ae2b229b4
Fabionunez <fabionunez@gmail.com>
2019-04-10 21:46:17 +0200

    all jasmine specks checked

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 'use strict';
 
-function main () {
+function main() {
   var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+    html += '<div class="card" data-card-name="' + pic.name + '">';
+    html += '  <div class="back" name="' + pic.img + '"></div>';
+    html += '  <div class="front" style="background: url(img/' + pic.img + ') no-repeat"></div>';
     html += '</div>';
   });
 
   // Add all the div's to the HTML
   var memoryBoard = document.querySelector('#memory_board');
-  if(memoryBoard) { // this condition is for passing the tests
+  if (memoryBoard) { // this condition is for passing the tests
     memoryBoard.innerHTML = html;
   }
-  
+
   // You will need to do something to the front as well
   var front = document.querySelectorAll('.front');
 
   // Bind the click event of each element to a function
-  var back = document.querySelector('.back');
-  back.addEventListener('click', function () {
-    // TODO: Your code goes here!
-  });
-  
-  
-  
+  var back = document.querySelectorAll('.back');
+  // back.addEventListener('click', function () {
+  //   console.log("hi");
+  // });
+
+
+
+
   // Helpers to create the logic of the game
   function turnBackCards() {
     setTimeout(function () {
@@ -37,24 +38,23 @@ function main () {
       prepareNextTurn();
     }, 1000);
   }
-  
+
   function prepareNextTurn() {
     memoryGame.pickedCards = [];
-    back.forEach((element)=> element.classList.remove('blocked'))
-    front.forEach((element)=> element.classList.remove('blocked'))
+    back.forEach((element) => element.classList.remove('blocked'))
+    front.forEach((element) => element.classList.remove('blocked'))
   }
-  
+
   function printGameInfo() {
     document.getElementById('pairs_clicked').innerHTML = memoryGame.pairsClicked;
     document.getElementById('pairs_guessed').innerHTML = memoryGame.pairsGuessed;
   }
-  
+
   function displayClickedCard(card) {
     card.className += ' active';
     card.style.background = 'url(img/' + card.getAttribute('name') + ') no-repeat';
   }
 };
 
+
 window.addEventListener('load', main);
-
-
