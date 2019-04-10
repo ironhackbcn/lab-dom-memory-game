4e441407a0df3d2aa3a6758cfebe7438e6bf8e22
Eric Capdevila <eric241@hotmail.es>
2019-04-10 21:58:15 +0200

    added shuffleCards function

--- src/memory.js
+++ src/memory.js
@@ -1,8 +1,23 @@
 var MemoryGame = function (cards) {
   this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = [];
+  this.pairsGuessed = [];
+
+
 };
 
-MemoryGame.prototype.shuffleCards = function () {
+MemoryGame.prototype.shuffleCards = function (unShuffledCards) {
+
+ let shuffledCards = [];
+ 
+ while(unShuffledCard.length > 0){
+  let randomCard = Math.floor(Math.random()*unShuffledCards.length);
+  shuffledCards.push(unShuffledCards.splice(randomCard, 1));
+ }
+
+ return shuffledCards;
+
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
@@ -10,3 +25,4 @@ MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
 MemoryGame.prototype.isFinished = function () {
 };
+
